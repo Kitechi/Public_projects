486741ff506453117ac82278ad6de98f4d7c24ae
Salahuddin <salahuddin.mrwt@gmail.com>
2022-10-08 15:43:53 +0900

    remove the VS Code directory from Git

--- VS Code/Python/01_Date Formate.py
+++
@@ -1,9 +0,0 @@
-import datetime as dt
-date_string = "25/12/2018"
-format = "%d/%m/%Y"
-
-try:
-  dt.datetime.strptime(date_string, format)
-  print("This is the correct date string format.")
-except ValueError:
-  print("This is the incorrect date string format. It should be YYYY-MM-DD")
